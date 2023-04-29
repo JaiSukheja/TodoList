5305f3a1593b943b3e79db3311dfe28d8b5f7f58
Jai Sukheja <jaisukheja02@gmail.com>
2023-04-29 20:11:47 +0530

    New Commit

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -12,7 +12,7 @@ const Task = (props) => {
     <div className="text-xl flex p-2 bg-slate-900 rounded-lg gap-3">
       <div className="flex w-1/15 gap-4 justify-start items-center">
         <div className="text-2xl p-1 rounded-lg px-4 text-white font-extrabold">
-          {props.idx+1}.
+          #{props.idx+1}
         </div>
       </div>
       <div className="flex w-3/4 items-center justify-around">
@@ -20,7 +20,7 @@ const Task = (props) => {
         <div className="text-blue-200 font-semibold overflow-hidden w-64">{props.desc}</div>
       </div>
       <div className="flex w-1/4 gap-4 justify-end">
-        <button className="bg-transparent p-1 rounded-lg px-4 text-rose-500 hover:bg-slate-800 gap-2 flex items-center" onClick={editIt} >
+        <button className="bg-transparent p-1 rounded-lg px-4 text-blue-600 hover:bg-slate-800 gap-2 flex items-center" onClick={editIt} >
         <i class="fa-regular fa-pen-to-square"></i>
           Edit
         </button>
